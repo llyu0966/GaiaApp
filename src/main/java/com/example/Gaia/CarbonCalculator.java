ac228151b8a98ed6289ef9cb5aa6aade3aaa9928
llyu0966 <47392852+llyu0966@users.noreply.github.com>
2020-09-26 05:07:08 -0400

    Create CarbonCalculator.java

---
+++ src/main/java/com/example/Gaia/CarbonCalculator.java
@@ -0,0 +1,10 @@
+package com.example.Gaia;
+
+/**
+ *
+ * @author linliyu
+ */
+// CarbonCalculator interface. File CarbonCalculator.java
+public interface CarbonCalculator {
+    double getCarbonFootprint();//calculates the carbon footprint
+}//end of interface
